6480f1ca07b4d4b127afa4236916d29024d6a60e
RomanPiat <108830304+RomanPiat@users.noreply.github.com>
2023-03-08 18:01:37 +0200

    4

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-
+export { fetchImages };
 
 const API_KEY= '33909260-89de06c1dbdf0a64f541db612';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
- export async function fetchImages(query, page, perPage) {
+async function fetchImages(query, page, perPage) {
 try {
-    const response = await axios.get(`?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+    const response = await axios.get(`?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,);
 
         return response;
 } catch (error) {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import {fetchImages} from './fetch';
 const submitSearch= document.getElementById('search-form')
 const listDivEl= document.querySelector('div')
-const fetchImages = new fetchImages();
+
 
 let query = ''
 let page = 1
@@ -20,10 +20,12 @@ function onSearchForm(e) {
 
         fetchImages(query, page, perPage) 
         .then(({data}) => {
-            alert('yes');
+
             if (data.totalHits === 0) {
                 alert('нет информации');
             }
+            console.log(data)
+
 
 
       
@@ -38,80 +40,3 @@ function onSearchForm(e) {
           };
         })
 }
-
-
-function renderSearchCard(searchCard) {
-    console.log('searchCard')
-}
-
-/* 
-
-// import './css/styles.css';
-//import debounce from 'lodash.debounce';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import fetchSearch from './fetch.js';
-
-// читаю форму
-const submitSearch= document.querySelector('#search-form')
-const listDivEl= document.querySelector('div')
-
-let query ='';
-let input ='';
-submitSearch.addEventListener('submit', onSearchForm)
-
-function onSearchForm(e) {
-    query = e.currentTarget.searchQuery.value.trim();
-    console.log(query)
-}
- */
-
-// evt => {
-//input = evt.target.value.trim(" ");
-
-
-
-/* fetchSearch(input) 
-.then(renderSearchCard) 
-.catch(error => {
-  console.log('eror catch', error.message)
-  if (error.message == 404){
-    Notify.failure("Oops, there is no country with that name")
-    refs.listDivEl.innerHTML = '';
-  };
-}) */
-
-
-
-
-/* 
-// function renderCountryCard(countryCard) {
-function renderSearchCard(searchCard) {
-  let markup = '';
-  
-  console.log(searchCard)
-  if (searchCard.length > 10) {
-    refs.listDivEl.innerHTML = '';
-    Notify.success("Too many matches found. Please enter a more specific name.")
-    return
-  }
-  else if (searchCard.length > 1) {
-     markup = markupCountries(searchCard);
-     refs.listEl.innerHTML = markup;
-     refs.listDivEl.innerHTML = '';
-  } 
-  else if (searchCard.length === 1) {
-     markup = markupCountry(searchCard);
-     refs.listDivEl.innerHTML = markup;
-  }
-}
-// форма вывода карточки
- function markupCountry(item) {
-  return item.map(item =>
-    `<h1><li class="header"><img src="${item.flag}" alt="flag" width="80" height ="60">
-  <p>  ${item.name}</p></h1><p> Capital: ${item.capital}</p><p> Population: ${item.population}</p><p> Languages: ${item.languages.map(lang => lang.name)}</p></li>`).join("");
-} 
-// форма вывода списка
-  function markupCountries(items) {
-  return items.map(item => `<li><img src="${item.flag}" alt="flag" width="40" height ="30">
-          <p>  ${item.name}</p></li>`).join("");
-}  */
